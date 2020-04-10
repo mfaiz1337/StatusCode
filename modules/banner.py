c58aedc2044c9e23077a553a208d25ce262b06d1
3P1CL33T <61756336+3P1CL33T@users.noreply.github.com>
2020-04-10 21:58:56 +0530

    Add files via upload

---
+++ modules/banner.py
@@ -0,0 +1,9 @@
+from modules.color import Fcolor
+banner = Fcolor.green+"""
+\_________               .__  .__
+\_   ___ \_______  ____ |  | |  |
+/    \  \/\_  __ \/  _ \|  | |  |
+\     \____|  | \(  <_> )  |_|  |__
+ \______  /|__|   \____/|____/____/
+        \/                   """+Fcolor.purple+'FAIZ'.center(0)
+
---
+++ modules/color.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class Fcolor:
+    black = "\033[1;30;1m"
+    white = "\033[1;37;1m"
+    red = "\033[1;31;1m"
+    cyan = "\033[1;36;1m"
+    purple = "\033[1;35;1m"
+    yellow = "\033[1;33;1m"
+    green = "\033[1;32;1m"
+    blue = "\033[1;34;1m"
+
+
+
+if __name__ == "__main__":
+    print(Fcolor.red+"Testing MOde on....")
