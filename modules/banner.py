930b6b9aa3880979b34ffa49fc903a9572414c02
3P1CL33T <61756336+3P1CL33T@users.noreply.github.com>
2020-04-13 18:42:24 +0530

    Delete banner.py

--- modules/banner.py
+++
@@ -1,9 +0,0 @@
-from modules.color import Fcolor
-banner = Fcolor.green+"""
-\_________               .__  .__
-\_   ___ \_______  ____ |  | |  |
-/    \  \/\_  __ \/  _ \|  | |  |
-\     \____|  | \(  <_> )  |_|  |__
- \______  /|__|   \____/|____/____/
-        \/                   """+Fcolor.purple+'FAIZ'.center(0)
-
