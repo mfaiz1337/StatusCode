a1b31ad9946e4e73a95ba6a5a38d850021f7d322
3P1CL33T <61756336+3P1CL33T@users.noreply.github.com>
2020-09-14 14:51:35 +0530

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,14 +1,2 @@
 from modules.color import Fcolor
 
-
-banner = Fcolor.green+r"""
-.d88888b           dP        888888ba  oo                   
-88.    "'          88        88    `8b       """+Fcolor.red+"""Coded By Faiz"""+Fcolor.green+"""               
-`Y88888b. dP    dP 88d888b. a88aaaa8P' dP 88d888b. .d8888b. 
-      `8b 88    88 88'  `88  88        88 88'  `88 88'  `88 
-d8'   .8P 88.  .88 88.  .88  88        88 88    88 88.  .88 
- Y88888P  `88888P' 88Y8888'  dP        dP dP    dP `8888P88 
-"""+Fcolor.white+"""          Tool Suggested By Anurag Kumar ( A1C3VENOM )"""+Fcolor.green+"""  .88 
-         """+Fcolor.white+"""Some Features By Rahul ( 5HAD0W )"""+Fcolor.green+"""          d8888P 
-"""
-
